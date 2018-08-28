a0ad6127b30437d7ee42208d84dc693efe250268
cavrau <39316655+cavrau@users.noreply.github.com>
2018-08-28 17:08:13 -0300

    fim do dia 28/08

--- The_Battle_Of_Gondria-master/game/src/scenes/Scene1_level1.js
+++ The_Battle_Of_Gondria-master/game/src/scenes/Scene1_level1.js
@@ -12,9 +12,9 @@ class Scene1_level1 extends Phaser.Scene{
         this.load.image('ceu', 'assets/background/sky_fase1.png');
         this.load.image('montanhas','assets/background/background_1.png');
         this.load.tilemapTiledJSON("map", "assets/tilemap/map_1.json");
-        // this.load.spritesheet('dude', 
-        //     'assets/dude.png',
-        //     { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('dude', 
+             'assets/images/dude.png',
+             { frameWidth: 32, frameHeight: 48 });
     
     }
     create(){
@@ -27,16 +27,44 @@ class Scene1_level1 extends Phaser.Scene{
         // Parameters: layer name (or index) from Tiled, tileset, x, y
         map.createStaticLayer('background', background,0,0);
         map.createStaticLayer('midground',midground,0,0);
-        let layer2= map.createStaticLayer("foreground_2",blocos,0,0);
-        let layer1 = map.createStaticLayer("foreground_1", blocos, 0, 0);
-        layer1.setCollisionBetween(2, 4);
-  
+        let layer2= map.createDynamicLayer("foreground_2",blocos,0,0);
+        let layer1 = map.createDynamicLayer("foreground_1", blocos, 0, 0);
+        layer1.setCollisionBetween(1, 7);
+        layer2.setCollisionBetween(2,7);
+
     //    const debugGraphics = this.add.graphics().setAlpha(0.75);
     //     layer1.renderDebug(debugGraphics, {
     //     tileColor: null, // Color of non-colliding tiles
     //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
     //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
     //   });
+        let player = this.physics.add.sprite(20,350,'dude');
+      
+        player.setBounce(0.1);
+    
+        player.setCollideWorldBounds(true);
+        this.player = player;
+        this.physics.add.collider(player, layer1);
+        this.physics.add.collider(player, layer2);
+        // this.cameras.main.startFollow(this.player);
+
+        // this.cameras.main.roundPixels = true;
+    }
+    update() {
+        let cursors = this.input.keyboard.createCursorKeys();
+        if(cursors.left.isDown){
+            this.player.setVelocityX(-160);
+            // this.player.anims.play('left',true);
+        }else if(cursors.right.isDown){
+            this.player.setVelocityX(160);
+            // this.player.anims.play('right',true);
+        }else{
+            // this.player.anims.play('turn',true);
+            this.player.setVelocityX(0);
+        }
+        if(cursors.up.isDown && this.player.body.onFloor()){
+        this.player.setVelocityY(-230);
+    }
     }
 
 }
