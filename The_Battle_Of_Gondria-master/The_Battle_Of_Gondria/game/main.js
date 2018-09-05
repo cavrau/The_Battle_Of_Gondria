cf598e2ec231fb1ec9b61c698971f7fec8b2f4af
Eduardo Augusto <34349837+eduaugustus@users.noreply.github.com>
2018-09-05 17:01:19 -0300

    DUDZUPDATE#3
    - Animação Main menu scene

---
+++ The_Battle_Of_Gondria-master/The_Battle_Of_Gondria/game/main.js
@@ -0,0 +1,26 @@
+/*INICIO DO JOGO*/
+import BootScene from './src/scenes/BootScene.js';
+import Scene1_level1 from './src/scenes/Scene1_level1.js';
+import MainMenu from './src/scenes/MainMenu.js'
+import MenuFases from './src/scenes/MenuFases.js'
+
+const config = {
+    type: Phaser.WEBGL,
+    parent: 'content',
+    width: 864,
+    height: 480,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 400 },
+            debug: false
+        }
+    },
+    scene: [
+        MainMenu,
+        MenuFases,
+        Scene1_level1
+    ]
+};
+
+const game = new Phaser.Game(config);
